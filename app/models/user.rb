ff962da134f201e3d83cd29d95916a31bec75db3
norikkun <161901725+norikkun@users.noreply.github.com>
2024-06-04 13:24:23 +0000

    サインアップ画面作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, length: { minimum: 6 }
 end
