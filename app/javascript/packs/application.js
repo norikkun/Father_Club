547fc8c36d385ec8f264055d172c104fa747d17c
norikkun <161901725+norikkun@users.noreply.github.com>
2024-06-08 08:59:41 +0000

    テスト前javascript修正2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,19 @@ import "../stylesheets/application";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// ヘッダーのスクロール動作
+const header = document.querySelector('header');
+let prevY = window.scrollY; 
+
+window.addEventListener('scroll', () => {
+  const currentY = window.scrollY;
+  if (currentY < prevY) { 
+    header.classList.remove('hidden'); 
+  } else { 
+    if (currentY > 0) {
+      header.classList.add('hidden'); 
+    }
+  }
+  prevY = currentY; 
+});
