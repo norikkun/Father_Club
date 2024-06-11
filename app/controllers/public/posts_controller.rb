0a4a8d9bf28b21340fb40b86cb1f105a61b54c82
norikkun <161901725+norikkun@users.noreply.github.com>
2024-06-11 05:00:01 +0000

    アクセス制限destroy追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,6 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
   
   def new
     @post = Post.new
