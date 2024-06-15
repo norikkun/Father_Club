64f53140c69e0c9f9c7ae00a0bf3cacd77c2f1b2
norikkun <161901725+norikkun@users.noreply.github.com>
2024-06-15 07:01:44 +0000

    [finish]いいね機能（非同期）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_13_061911) do
+ActiveRecord::Schema.define(version: 2024_06_04_065010) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2024_06_13_061911) do
   end
 
   create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
