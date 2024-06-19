61bcbf7ce5ba02e2e4f4f730900e5271d9be1c1f
norikkun <161901725+norikkun@users.noreply.github.com>
2024-06-19 05:21:50 +0000

    レイアウト修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -28,7 +28,7 @@ class Public::UsersController < ApplicationController
     @user = User.find(params[:user_id])
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
-end
+  end
 
   def follower
   end
