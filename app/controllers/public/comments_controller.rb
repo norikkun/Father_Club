c8285fbbdb494a10f873e973b5c86e9b4857fe59
norikkun <161901725+norikkun@users.noreply.github.com>
2024-06-26 11:39:13 +0000

    コメントバリデーションエラー修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,9 +5,7 @@ class Public::CommentsController < ApplicationController
     post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = post.id
-    if @comment.save
-      flash[:notice] = "コメントを投稿しました"
-    else
+    unless @comment.save
      flash[:notice] = "コメントを入力してください"
      render 'error'
     end
